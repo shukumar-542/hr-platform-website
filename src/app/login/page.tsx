357c4ca0ff5b5f751eec149f557f7bf35f81cb57
shukumar-542 <shukumar542@gmail.com>
2025-03-31 15:32:39 +0600

    Implement login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
-
-const LoginPage = () => {
-  return (
-    <div className=''>LoginPage</div>
-  )
+"use client";
+import React from "react";
+import bg from "../../assets/background.png";
+import { Form, Input } from "antd";
+import Button from '../../components/Button/Button'
+interface LoginFormValues {
+  email : string;
+  password : string
 }
+const LoginPage = () => {
 
-export default LoginPage
+  const handleLogin = (values : LoginFormValues)=>{
+    console.log(values);
+  }
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${bg.src})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+      className="flex justify-end h-screen"
+    >
+      <div className="container mx-auto px-2 md:px-0 flex justify-end items-center">
+        <div className="bg-white p-6 rounded-lg shadow-lg  md:min-w-[500px]">
+          <p className="text-center text-xl md:text-4xl font-semibold text-[#333333]">Login to Account</p>
+          <p className="text-center text-sm mt-2 text-[#333333]">
+          Please enter your email and password to continue
+          </p>
+
+          {/* Ensure Form has onFinish handler */}
+          <Form layout="vertical" onFinish={handleLogin}>
+            <Form.Item name={"email"} label={<p className="text-[#333333] mt-5">Email Address</p>}>
+              <Input placeholder="esteban_schiller@gmail.com" className="border-red-400 border" />
+            </Form.Item>
+            <Form.Item name={"Password"} label={<p className="text-[#333333]">Password</p>}>
+              <Input.Password placeholder="************" className="border-red-400 border" />
+            </Form.Item>
+            <Button>Login</Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react'
+
+interface IButtonProps {
+    children: ReactNode;
+}
+
+const Button: React.FC<IButtonProps> = ( {children}) => {
+  return (
+    <button className='bg-[#2A216D] text-white w-full rounded-sm shadow-2xl py-2 cursor-pointer'>{children}</button>
+  )
+}
+
+export default Button
