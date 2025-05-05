3b8d755b3ffc87a5ae575b03c50f1b6d06bdcb75
shukumar-542 <shukumar542@gmail.com>
2025-05-05 13:47:49 +0600

    Implement shift swift request page

--- src/app/(withCommonLayout)/shift-swap-request/page.tsx
+++ src/app/(withCommonLayout)/shift-swap-request/page.tsx
@@ -1,12 +1,35 @@
 import { BackButton } from '@/components/BackButton/BackButton'
 import ShiftSwapRequest from '@/components/ShiftSwapRequest/ShiftSwapRequest'
 import React from 'react'
+import emp2 from '@/assets/emp2.png'
 
 const ShiftSwapPage = () => {
+    const shiftData = [
+        {
+          key: "1",
+          slNo: "#1233",
+          date: "12/03/25",
+          employee: "Annette Black",
+          image: emp2,
+          currentShift: "7 AM - 3 PM",
+          requestedSwap: "11 PM - 7 AM",
+          status: "approved",
+        },
+        {
+          key: "2",
+          slNo: "#1233",
+          date: "12/03/25",
+          employee: "Ronald Richards",
+          image: emp2,
+          currentShift: "3 PM - 11 PM",
+          requestedSwap: "7 AM - 3 PM",
+          status: "declined",
+        },
+      ];
   return (
     <div className='bg-white p-4 rounded-md min-h-[87vh]'>
         <BackButton title='Shift Swap Request' />
-        {/* <ShiftSwapRequest/> */}
+        <ShiftSwapRequest shiftData={shiftData} />
     </div>
   )
 }
