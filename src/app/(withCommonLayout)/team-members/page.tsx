899ea6091f59829dfcb79d5c5f009dca951e0921
shukumar-542 <shukumar542@gmail.com>
2025-05-06 08:49:18 +0600

    Implement edit and details modal

--- src/app/(withCommonLayout)/team-members/page.tsx
+++ src/app/(withCommonLayout)/team-members/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { BackButton } from "@/components/BackButton/BackButton";
 import SearchBar from "@/components/SearchBar/SearchBar";
-import { Table } from "antd";
+import { Modal, Table } from "antd";
 import React, { useState } from "react";
 import { CiEdit } from "react-icons/ci";
 import { IoEyeOutline } from "react-icons/io5";
@@ -18,6 +18,8 @@ interface DataType {
 }
 
 const TeamMembersPage = () => {
+  const [openDetailsModal, setOpenDetailsModal] = useState(false);
+  const [openEditModal, setOpenEditModal] = useState(false);
   const [searchValue, setSearchValue] = useState("");
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -109,7 +111,7 @@ const TeamMembersPage = () => {
       dataIndex: "enrollToTraining",
       render: () => {
         return (
-          <button className="bg-red-500 text-white p-2 rounded-sm cursor-pointer">
+          <button onClick={()=> setOpenEditModal(true)} className="bg-red-500 text-white p-2 rounded-sm cursor-pointer">
             <CiEdit size={20} />
           </button>
         );
@@ -120,12 +122,53 @@ const TeamMembersPage = () => {
       dataIndex: "details",
       render: () => {
         return (
-          <button className="bg-[#03346E] text-white p-2 rounded-sm cursor-pointer">
+          <button
+            onClick={() => setOpenDetailsModal(true)}
+            className="bg-[#03346E] text-white p-2 rounded-sm cursor-pointer"
+          >
             <IoEyeOutline size={20} />
           </button>
         );
       },
     },
+  ];
+
+  // Edit modal data
+  const [selectedTrainings, setSelectedTrainings] = useState<string[]>([
+    "Compliance & Ethics Training",
+    "Leadership & Team Management",
+    "Time Management & Productivity",
+    "HR & Payroll Compliance Training",
+    "Diversity, Equity, & Inclusion (DEI) Training",
+    "Cybersecurity Awareness Training",
+    "Conflict Resolution & Workplace Mediation",
+    "Payroll Compliance Training",
+  ]);
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>,
+    training: string) => {
+    if (event.target.checked) {
+      setSelectedTrainings([...selectedTrainings, training]);
+    } else {
+      setSelectedTrainings(
+        selectedTrainings.filter((item) => item !== training)
+      );
+    }
+  };
+
+  const availableTrainings = [
+    'Workplace Safety & Emergency Procedures',
+    'Compliance & Ethics Training',
+    'Leadership & Team Management',
+    'Time Management & Productivity',
+    'HR & Payroll Compliance Training',
+    'Customer Service & Communication Skills',
+    'Diversity, Equity, & Inclusion (DEI) Training',
+    'Cybersecurity Awareness Training',
+    'Conflict Resolution & Workplace Mediation',
+    'Software & Technology Training',
+    'Payroll Compliance Training',
+    'Annual Safety Training',
   ];
 
   return (
@@ -136,6 +179,85 @@ const TeamMembersPage = () => {
       </div>
 
       <Table columns={columns} dataSource={data} pagination={false} />
+
+      {/* Team Member details modal */}
+      <Modal
+        open={openDetailsModal}
+        onCancel={() => setOpenDetailsModal(false)}
+        centered
+        footer={false}
+      >
+        <h1 className="text-center text-xl font-semibold">Details</h1>
+        <div className="p-6 font-sans">
+          <div className="mb-4 flex justify-between items-center">
+            <strong className="font-medium">Team Member Name:</strong>
+            <div>Annette Black</div>
+          </div>
+
+          <div className="mb-4 flex justify-between items-center">
+            <strong className="font-medium">Job Title:</strong>
+            <div>Financial Analyst</div>
+          </div>
+
+          <div className="mb-4 flex justify-between items-center">
+            <strong className="font-medium">Department:</strong>
+            <div>Finance</div>
+          </div>
+
+          <div className="mb-4 flex justify-between items-center">
+            <strong className="font-medium">Employment Status:</strong>
+            <div>Full-Time</div>
+          </div>
+
+          <div className="mb-4">
+            <strong className="font-medium">Enrolled Training:</strong>
+            <ul className="list-disc pl-5">
+              <li>Compliance & Ethics Training</li>
+              <li>Workplace Safety & Emergency Procedures</li>
+              <li>Leadership & Team Management</li>
+              <li>HR & Payroll Compliance Training</li>
+              <li>Conflict Resolution & Workplace Mediation</li>
+            </ul>
+          </div>
+        </div>
+      </Modal>
+
+      {/* Enroll training edit modal */}
+      <Modal
+        open={openEditModal}
+        onCancel={() => setOpenEditModal(false)}
+        centered
+        footer={false}
+      >
+          <div className="p-6 font-sans max-w-xl mx-auto">
+      <div className="space-y-4">
+        {availableTrainings.map((training) => (
+          <div key={training} className="flex items-center">
+            <input
+              type="checkbox"
+              checked={selectedTrainings.includes(training)}
+              onChange={(e) => handleCheckboxChange(e, training)}
+              className="mr-2 accent-[#03346E]"
+            />
+            <label className="text-lg">{training}</label>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex justify-between gap-5 mt-6">
+        <button
+          className="px-4 py-2 bg-white border border-gray-300 rounded-md text-gray-700 hover:bg-gray-100 w-full"
+        >
+          Cancel
+        </button>
+        <button
+          className="px-4 py-2 bg-[#03346E] text-white rounded-md w-full"
+        >
+          Save
+        </button>
+      </div>
+    </div>
+      </Modal>
     </div>
   );
 };
