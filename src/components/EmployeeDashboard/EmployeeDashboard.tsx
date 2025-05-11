7f7f4f536177eb1795b4951861848a71430f9fa3
shukumar-542 <shukumar542@gmail.com>
2025-05-11 09:10:29 +0600

    Implement clock for empolyee

--- src/components/EmployeeDashboard/EmployeeDashboard.tsx
+++ src/components/EmployeeDashboard/EmployeeDashboard.tsx
@@ -5,8 +5,10 @@ import Image from "next/image";
 import manager from "@/assets/manager.png";
 import { FcCalendar } from "react-icons/fc";
 import Link from "next/link";
+import clock from "@/assets/clock.png";
 
 const EmployeeDashboard = () => {
+  const progress = 38.12;
   return (
     <div>
       <div className="bg-white flex items-center gap-3 p-2 rounded-sm">
@@ -22,14 +24,46 @@ const EmployeeDashboard = () => {
 
       {/* Employee attendance and time summary section */}
       <div className="grid grid-cols-1 md:grid-cols-12 gap-6 mt-5">
-        <div className="col-span-12 md:col-span-4 bg-white rounded-md p-4">
+        <div className="col-span-12 md:col-span-4 bg-white rounded-md p-6">
           <div className="flex justify-between items-center">
             <p>8 April, 2025, Tuesday.</p>
             <p>
               Clocked in: <span className="font-semibold"> 9:02 AM</span>
             </p>
           </div>
-          <div className="relative"></div>
+          {/* Clock  */}
+          <div className="flex items-center justify-between h-full">
+            <button className="bg-[#0A7900] rounded-sm text-white px-2 py-1 cursor-pointer">
+              Clock In
+            </button>
+            <div className="flex flex-col justify-center items-center">
+              <div
+                className="relative h-52 w-52 rounded-full"
+                style={{
+                  background: `conic-gradient(#EC7C25 ${progress}%, #03346E ${progress}% 100%)`,
+                }}
+              >
+                <div className="absolute top-[12.5%] left-[12.5%] h-[75%] w-[75%] bg-white rounded-full flex items-center justify-center">
+                  <Image
+                    src={clock}
+                    alt="clock"
+                    className="w-32 h-32 object-contain"
+                  />
+                  <p className="absolute text-[#03346E] font-bold text-sm">
+                    4 H 35 Min
+                  </p>
+                </div>
+              </div>
+              <div>
+                <button className="mt-5 bg-[#A1A1A1] rounded-sm px-4 py-2 cursor-pointer">
+                  Break Time
+                </button>
+              </div>
+            </div>
+            <button className="bg-red-600 rounded-sm text-white px-2 py-1 cursor-pointer">
+              Clock Out
+            </button>
+          </div>
         </div>
 
         {/* Training and application summery */}
